467ae8e7a75d85b9b727567af8673b212a9997cf
Adithya Venkatesh <adithyavenkatesh.in@gmail.com>
2024-02-24 00:58:53 +0530

    Made FloodFill code more modular

--- FloodFill.c
+++ FloodFill.c
@@ -1,34 +1,63 @@
 #include <stdio.h>
-
+#include <string.h>
 #include "API.h"
 
-void log(char* text) {
+#define maxDigits 3
+#define size 16
+
+// Have to reduce this
+#define queueCapacity 256
+
+void log(char *text)
+{
     fprintf(stderr, "%s\n", text);
     fflush(stderr);
 }
 
-struct cell {
+char numAsString[maxDigits];
+char *numToString(int x)
+{
+    // Works only for whole numbers
+    int i = maxDigits - 1;
+
+    while (i >= 0)
+    {
+        if (x > 0)
+        {
+            numAsString[i] = x % 10 + 48;
+            x /= 10;
+        }
+        else
+            numAsString[i] = '0';
+        i--;
+    }
+
+    return numAsString;
+}
+
+struct cell
+{
     int row;
     int col;
 };
 
-struct cell goals[4] = {
-    {7, 7}, {7, 8}, {8, 7}, {8, 8}
-};
-
-struct cell floodQueue[32];
+struct cell floodQueue[queueCapacity];
 int frontFQ = -1, rearFQ = -1;
 
 // Time Complexity - Big O(1)
-void insertCell(int row, int col) {
+void insertCell(int row, int col)
+{
     struct cell C = {row, col};
 
-    if ((frontFQ == 0 && rearFQ == 31) || rearFQ + 1 == frontFQ) {
+    if ((frontFQ == 0 && rearFQ == queueCapacity - 1) || rearFQ + 1 == frontFQ)
+    {
         log("Flood Queue overflow");
-    } else {
-        if (rearFQ == -1) 
+    }
+    else
+    {
+        if (rearFQ == -1)
             frontFQ = 0, rearFQ = 0;
-        else if (rearFQ == 31)    
+        else if (rearFQ == queueCapacity - 1)
             rearFQ = 0;
         else
             rearFQ++;
@@ -36,16 +65,20 @@ void insertCell(int row, int col) {
     }
 }
 
-struct cell removeCell() {
+struct cell removeCell()
+{
     struct cell C;
 
-    if (frontFQ == -1) {
+    if (frontFQ == -1)
+    {
         log("Flood Queue underflow");
-    } else {
+    }
+    else
+    {
         C = floodQueue[frontFQ];
-        if (frontFQ == rearFQ) 
+        if (frontFQ == rearFQ)
             frontFQ = -1, rearFQ = -1;
-        else if (frontFQ == 31)
+        else if (frontFQ == queueCapacity - 1)
             frontFQ = 0;
         else
             frontFQ++;
@@ -54,28 +87,48 @@ struct cell removeCell() {
     return C;
 }
 
-int hWalls[15][16];
-int vWalls[16][15];
+int hWalls[size - 1][size];
+int vWalls[size][size - 1];
 
-int maze[16][16];
+int maze[size][size];
 
-void initializeDists() {
-    for (int r = 0; r < 16; r++) {
-        for (int c = 0; c < 16; c++) {
+void initializeDists()
+{
+    for (int r = 0; r < size; r++)
+    {
+        for (int c = 0; c < size; c++)
+        {
             maze[r][c] = -1;
         }
     }
 
-    for (int i = 0; i < 4; i++) {
-        maze[goals[i].row][goals[i].col] = 0;
+    if (size % 2 == 0)
+    {
+        maze[size / 2 - 1][size / 2 - 1] = 0;
+        maze[size / 2][size / 2 - 1] = 0;
+        maze[size / 2 - 1][size / 2] = 0;
+        maze[size / 2][size / 2] = 0;
+    }
+    else
+    {
+        maze[size / 2][size / 2] = 0;
     }
 }
 
-void floodfill() {
+void floodfill()
+{
     initializeDists();
 
-    for (int i = 0; i < 4; i++) {
-        insertCell(goals[i].row, goals[i].col);
+    if (size % 2 == 0)
+    {
+        insertCell(size / 2 - 1, size / 2 - 1);
+        insertCell(size / 2 - 1, size / 2);
+        insertCell(size / 2, size / 2 - 1);
+        insertCell(size / 2, size / 2);
+    }
+    else
+    {
+        insertCell(size / 2, size / 2);
     }
 
     while (!(frontFQ == -1 && rearFQ == -1))
@@ -84,44 +137,238 @@ void floodfill() {
         int r = C.row, c = C.col;
         int val = maze[r][c];
 
+        API_setText(c, 15 - r, numToString(val));
+
         // Updating North Neighbour
-        if (c > 0 && maze[r][c - 1] == -1 && vWalls[r][c - 1] != 1) {
+        if (c > 0 && maze[r][c - 1] == -1 && vWalls[r][c - 1] != 1)
+        {
             maze[r][c - 1] = val + 1;
             insertCell(r, c - 1);
         }
 
         // Updating South Neighbour
-        if (c < 15 && maze[r][c + 1] == -1 && vWalls[r][c] != 1) {
+        if (c < 15 && maze[r][c + 1] == -1 && vWalls[r][c] != 1)
+        {
             maze[r][c + 1] = val + 1;
             insertCell(r, c + 1);
         }
 
         // Updating West Neighbour
-        if (r > 0 && maze[r - 1][c] == -1 && hWalls[r - 1][c] != 1) {
+        if (r > 0 && maze[r - 1][c] == -1 && hWalls[r - 1][c] != 1)
+        {
             maze[r - 1][c] = val + 1;
             insertCell(r - 1, c);
         }
 
         // Updating East Neighbour
-        if (r < 15 && maze[r + 1][c] == -1 && hWalls[r][c] != 1) {
+        if (r < 15 && maze[r + 1][c] == -1 && hWalls[r][c] != 1)
+        {
             maze[r + 1][c] = val + 1;
             insertCell(r + 1, c);
         }
-    }   
+    }
 }
 
-int isGoal(int row, int col) {
-    for (int i = 0; i < 4; i++) {
-        if (goals[i].row == row && goals[i].col == col) {
-            return 1;
+int isGoal(int row, int col)
+{
+    if (size % 2 == 0)
+    {
+        return (row == size / 2 || row == size / 2 - 1) && (col == size / 2 || col == size / 2 - 1);
+    }
+    else
+    {
+        return row == size / 2 && col == size / 2;
+    }
+}
+
+int cellExists(int r, int c, int dir)
+{
+    switch (dir)
+    {
+    case 0:
+        return r > 0;
+
+    case 1:
+        return c < size - 1;
+
+    case 2:
+        return r < size - 1;
+
+    case 3:
+        return c > 0;
+    }
+}
+
+int getCellVal(int r, int c, int dir)
+{
+    switch (dir)
+    {
+    case 0:
+        return maze[r - 1][c];
+
+    case 1:
+        return maze[r][c + 1];
+
+    case 2:
+        return maze[r + 1][c];
+
+    case 3:
+        return maze[r][c - 1];
+    }
+}
+
+int getWall(int r, int c, int dir)
+{
+    switch (dir)
+    {
+    case 0:
+        return hWalls[r - 1][c];
+
+    case 1:
+        return vWalls[r][c];
+
+    case 2:
+        return hWalls[r][c];
+
+    case 3:
+        return vWalls[r][c - 1];
+    }
+}
+
+void setWall(int r, int c, int dir, int val)
+{
+    switch (dir)
+    {
+    case 0:
+        hWalls[r - 1][c] = val;
+        break;
+
+    case 1:
+        vWalls[r][c] = val;
+        break;
+
+    case 2:
+        hWalls[r][c] = val;
+        break;
+
+    case 3:
+        vWalls[r][c - 1] = val;
+        break;
+    }
+}
+
+int wallPresent(int side)
+{
+    switch (side)
+    {
+    case 0:
+        return API_wallFront();
+
+    case 1:
+        return API_wallRight();
+
+    case 3:
+        return API_wallLeft();
+    }
+}
+
+void updateFinalCellCoordinates(int r, int c, int *rfPtr, int *cfPtr, int dir)
+{
+    switch (dir)
+    {
+    case 0:
+        *rfPtr = r - 1;
+        break;
+
+    case 1:
+        *cfPtr = c + 1;
+        break;
+
+    case 2:
+        *rfPtr = r + 1;
+        break;
+
+    case 3:
+        *cfPtr = c - 1;
+        break;
+    }
+}
+
+char directionAsLetter(int dir) {
+    switch (dir)
+    {
+    case 0:
+        return 'n';
+        break;
+    
+    case 1:
+        return 'e';
+        break;
+    
+    case 2:
+        return 's';
+        break;
+    
+    case 3:
+        return 'w';
+        break;
+    }
+}
+
+void checkWall(int r, int c, int val, int orient, int side, int *actionPtr, int *rfPtr, int *cfPtr)
+{
+    int dir = (orient + side) % 4;
+
+    if (side == 2)
+    {
+        if (*actionPtr == -1 && getCellVal(r, c, dir) == val - 1)
+        {
+            *actionPtr = side;
+            updateFinalCellCoordinates(r, c, rfPtr, cfPtr, dir);
         }
     }
-    return 0;
+    else
+    {
+        if (cellExists(r, c, dir))
+        {
+            if (getWall(r, c, dir) == 0)
+            {
+                if (wallPresent(side)) {
+                    setWall(r, c, dir, 1);
+                    API_setWall(c, 15 - r, directionAsLetter(dir));
+                } 
+                else
+                    setWall(r, c, dir, -1);
+            }
+
+            if (*actionPtr == -1 && getWall(r, c, dir) == -1 && getCellVal(r, c, dir) == val - 1)
+            {
+                *actionPtr = side;
+                updateFinalCellCoordinates(r, c, rfPtr, cfPtr, dir);
+            }
+        }
+    }
 }
 
-int main(int argc, char* argv[]) {
-    for (int i = 0; i < 15; i++) {
-        for (int j = 0; j < 16; j++) {
+void performTurn(int action)
+{
+    if (action == 1)
+        API_turnRight();
+    else if (action == 2)
+    {
+        API_turnRight();
+        API_turnRight();
+    }
+    else if (action == 3)
+        API_turnLeft();
+}
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
             hWalls[i][j] = 0;
             vWalls[j][i] = 0;
         }
@@ -132,221 +379,26 @@ int main(int argc, char* argv[]) {
 
     floodfill();
 
-    while (!isGoal(r, c)) {
+    while (!isGoal(r, c))
+    {
         int val = maze[r][c];
         int action = -1;
         int rf = r, cf = c;
 
-        // North
-        if (orientation == 0) {
-            // Check Front (North)
-            if (r > 0) {
-                if (hWalls[r - 1][c] == 0) {
-                    if (API_wallFront()) hWalls[r - 1][c] = 1;
-                    else hWalls[r - 1][c] = -1;
-                }
+        checkWall(r, c, val, orientation, 0, &action, &rf, &cf);
+        checkWall(r, c, val, orientation, 1, &action, &rf, &cf);
+        checkWall(r, c, val, orientation, 3, &action, &rf, &cf);
+        checkWall(r, c, val, orientation, 2, &action, &rf, &cf);
 
-                if (hWalls[r - 1][c] == -1 && maze[r - 1][c] == val - 1) {
-                    action = 0;
-                    rf = r - 1;
-                }
-            }
-
-            // Check Right (East)
-            if (c < 15) {
-                if (vWalls[r][c] == 0) {
-                    if (API_wallRight()) vWalls[r][c] = 1;
-                    else vWalls[r][c] = -1;
-                }
-
-                if (action == -1 && vWalls[r][c] == -1 && maze[r][c + 1] == val - 1) {
-                    action = 1;
-                    cf = c + 1;
-                }
-            }
-
-            // Check Left (West)
-            if (c > 0) {
-                if (vWalls[r][c - 1] == 0) {
-                    if (API_wallLeft()) vWalls[r][c - 1] = 1;
-                    else vWalls[r][c - 1] = -1;
-                }
-
-                if (action == -1 && vWalls[r][c - 1] == -1 && maze[r][c - 1] == val - 1) {
-                    action = 3;
-                    cf = c - 1;
-                }
-            }
-
-            // Check Backtrack (South)
-            if (action == -1 && maze[r + 1][c] == val - 1) {
-                action = 2;
-                rf = r + 1;
-            }
-        }
-
-        // East
-        else if (orientation == 1) {
-            // Check Front (East)
-            if (c < 15) {
-                if (vWalls[r][c] == 0) {
-                    if (API_wallFront()) vWalls[r][c] = 1;
-                    else vWalls[r][c] = -1;
-                }
-
-                if (vWalls[r][c] == -1 && maze[r][c + 1] == val - 1) {
-                    action = 0;
-                    cf = c + 1;
-                }
-            }
-
-            // Check Right (South)
-            if (r  < 15) {
-                if (hWalls[r][c] == 0) {
-                    if (API_wallRight()) hWalls[r][c] = 1;
-                    else hWalls[r][c] = -1;
-                }
-
-                if (action == -1 && hWalls[r][c] == -1 && maze[r + 1][c] == val - 1) {
-                    action = 1;
-                    rf = r + 1;
-                }
-            }
-
-            // Check Left (North)
-            if (r > 0) {
-                if (hWalls[r - 1][c] == 0) {
-                    if (API_wallLeft()) hWalls[r - 1][c] = 1;
-                    else hWalls[r - 1][c] = -1;
-                }
-
-                if (action == -1 && hWalls[r - 1][c] == -1 && maze[r - 1][c] == val - 1) {
-                    action = 3;
-                    rf = r - 1;
-                }
-            }
-
-            // Check Backtrack (West)
-            if (action == -1 && maze[r][c - 1] == val - 1) {
-                action = 2;
-                cf = c - 1;
-            }
-        }
-
-        // South
-        else if (orientation == 2) {
-            // Check Front (South)
-            if (r  < 15) {
-                if (hWalls[r][c] == 0) {
-                    if (API_wallFront()) hWalls[r][c] = 1;
-                    else hWalls[r][c] = -1;
-                }
-
-                if (hWalls[r][c] == -1 && maze[r + 1][c] == val - 1) {
-                    action = 0;
-                    rf = r + 1;
-                }
-            }
-
-            // Check Right (West)
-            if (c > 0) {
-                if (vWalls[r][c - 1] == 0) {
-                    if (API_wallRight()) vWalls[r][c - 1] = 1;
-                    else vWalls[r][c - 1] = -1;
-                }
-
-                if (action == -1 && vWalls[r][c - 1] == -1 && maze[r][c - 1] == val - 1) {
-                    action = 1;
-                    cf = c - 1;
-                }
-            }
-
-            // Check Left (East)
-            if (c < 15) {
-                if (vWalls[r][c] == 0) {
-                    if (API_wallLeft()) vWalls[r][c] = 1;
-                    else vWalls[r][c] = -1;
-                }
-
-                if (action == -1 && vWalls[r][c] == -1 && maze[r][c + 1] == val - 1) {
-                    action = 3;
-                    cf = c + 1;
-                }
-            }
-
-            // Check Backtrack (North)
-            if (action == -1 && maze[r - 1][c] == val - 1) {
-                action = 2;
-                rf = r - 1;
-            }
-        }
-
-        // West
-        else {
-            // Check Front (West)
-            if (c > 0) {
-                if (vWalls[r][c - 1] == 0) {
-                    if (API_wallFront()) vWalls[r][c - 1] = 1;
-                    else vWalls[r][c - 1] = -1;
-                }
-
-                if (vWalls[r][c - 1] == -1 && maze[r][c - 1] == val - 1) {
-                    action = 0;
-                    cf = c - 1;
-                }
-            }
-
-            // Check Right (North)
-            if (r > 0) {
-                if (hWalls[r - 1][c] == 0) {
-                    if (API_wallRight()) hWalls[r - 1][c] = 1;
-                    else hWalls[r - 1][c] = -1;
-                }
-
-                if (action == -1 && hWalls[r - 1][c] == -1 && maze[r - 1][c] == val - 1) {
-                    action = 1;
-                    rf = r - 1;
-                }
-            }
-
-            // Check Left (South)
-            if (r  < 15) {
-                if (hWalls[r][c] == 0) {
-                    if (API_wallLeft()) hWalls[r][c] = 1;
-                    else hWalls[r][c] = -1;
-                }
-
-                if (action == -1 && hWalls[r][c] == -1 && maze[r + 1][c] == val - 1) {
-                    action = 3;
-                    rf = r + 1;
-                }
-            }
-    
-            // Check Backtrack (East)
-            if (action == -1 && maze[r][c + 1] == val - 1) {
-                action = 2;
-                cf = c + 1;
-            }
-        }
-
-        if (action == -1) {
+        if (action == -1)
+        {
             floodfill();
-        } else {
-            if (action == 1) {
-                API_turnRight();
-                orientation += 1;
-                if (orientation > 3) orientation = 0;
-            } else if (action == 2) {
-                API_turnRight(); API_turnRight();
-                if (orientation < 2) orientation += 2;
-                else orientation -= 2;
-            } else if (action == 3) {
-                API_turnLeft();
-                orientation -= 1;
-                if (orientation < 0) orientation = 3;
-            }
-
+        }
+        else
+        {
+            performTurn(action);
             API_moveForward();
+            orientation = (orientation + action) % 4;
         }
 
         r = rf, c = cf;
